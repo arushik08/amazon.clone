be0579f8ff1ac2617444842720ce50068f8c70f5
arushik08 <137804437+arushik08@users.noreply.github.com>
2023-10-05 19:26:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+document.querySelector(".r-btn").addEventListener("click",{
+    function(event){
+        document.querySelector(".product-slide").scrollLeft+=1100;
+        event.preventDefault();
+    }
+})
